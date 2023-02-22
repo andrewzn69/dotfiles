4f12161ce7a1d8c1e8a0e08a117e9f5f8ed70646
andrewzn69 <ondrejzeman23@gmail.com>
2023-02-22 01:01:09 +0100

    feat(i3 & rofi): rofi power menu init
    switched to rofi power menu

---
+++ .config/rofi/powermenu.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+
+# Current Theme
+dir="$HOME/.config/rofi/"
+theme='catppuccin-mocha-powermenu'
+
+# CMDs
+uptime="`uptime -p | sed -e 's/up //g'`"
+
+# Options
+hibernate=''
+shutdown=''
+reboot=''
+lock=''
+suspend=''
+logout=''
+yes=''
+no=''
+
+# Rofi CMD
+rofi_cmd() {
+	rofi -dmenu \
+		-mesg "Uptime: $uptime" \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Confirmation CMD
+confirm_cmd() {
+	rofi -theme-str 'window {location: center; anchor: center; fullscreen: false; width: 350px;}' \
+		-theme-str 'mainbox {children: [ "message", "listview" ];}' \
+		-theme-str 'listview {columns: 2; lines: 1;}' \
+		-theme-str 'element-text {horizontal-align: 0.5;}' \
+		-theme-str 'textbox {horizontal-align: 0.5;}' \
+		-dmenu \
+		-theme ${dir}/${theme}.rasi
+}
+
+# Pass variables to rofi dmenu
+run_rofi() {
+  echo -e "$suspend\n$reboot\n$shutdown\n$lock\n$logout\n$hibernate" | rofi_cmd
+}
+
+# Execute Command
+run_cmd() {
+	if [[ "$selected" == "$yes" ]]; then
+		if [[ $1 == '--shutdown' ]]; then
+			systemctl poweroff
+		elif [[ $1 == '--reboot' ]]; then
+			systemctl reboot
+		elif [[ $1 == '--hibernate' ]]; then
+			systemctl hibernate
+		elif [[ $1 == '--suspend' ]]; then
+
+			mpc -q pause
+			amixer set Master mute
+			systemctl suspend
+		elif [[ $1 == '--logout' ]]; then
+			if [[ "$DESKTOP_SESSION" == 'openbox' ]]; then
+				openbox --exit
+			elif [[ "$DESKTOP_SESSION" == 'bspwm' ]]; then
+				bspc quit
+			elif [[ "$DESKTOP_SESSION" == 'i3' ]]; then
+				i3-msg exit
+			elif [[ "$DESKTOP_SESSION" == 'plasma' ]]; then
+				qdbus org.kde.ksmserver /KSMServer logout 0 0 0
+			fi
+		fi
+	else
+		exit 0
+	fi
+}
+
+# Actions
+chosen="$(run_rofi)"
+case ${chosen} in
+    $shutdown)
+		run_cmd --shutdown
+        ;;
+    $reboot)
+		run_cmd --reboot
+        ;;
+    $hibernate)
+		run_cmd --hibernate
+        ;;
+    $lock)
+		if [[ -x '/usr/bin/betterlockscreen' ]]; then
+			betterlockscreen -l
+		elif [[ -x '/usr/bin/i3lock' ]]; then
+			i3lock
+		fi
+        ;;
+    $suspend)
+    bash $HOME/Development/bash-scripts/openrgb2.sh
+    i3exit suspend
+        ;;
+    $logout)
+		run_cmd --logout
+        ;;
+esac
