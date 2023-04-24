144aff0141a136a1807efa8c6f42a92fcff82232
andrewzn <ondrejzeman23@gmail.com>
2023-04-24 08:35:37 +0200

    fix(playerctl.sh): fixed playerctl not showing current media played

--- .config/waybar/scripts/playerctl.sh
+++ .config/waybar/scripts/playerctl.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+FORMAT="{{ title }} - {{ artist }}"
+
 playerctl_status=$(playerctl status 2>/dev/null)
 
 if [[ $playerctl_status == "Playing" ]]; then
-    title=$(playerctl metadata title 2>/dev/null)
-    echo '{"text":"󰎈 󰏤","class":"playing","title":"$title"}'
+    playerctl --player=$PLATER metadata --format "$FORMAT"
 elif [[ $playerctl_status == "Paused" ]]; then
-    title=$(playerctl metadata title 2>/dev/null)
-    echo '{"text":"󰎈 󰐊","class":"paused","title":"$title"}'
+    playerctl --player=$PLATER metadata --format "$FORMAT"
 else
     echo 'No Player Found'
 fi
