0005c7484582d55b51d91c8de40856360c5fbc54
andrewzn <ondrejzeman23@gmail.com>
2023-09-18 10:07:39 +0200

    feat(zsh): add --noconfirm to yay aliases

---
+++ .config/zsh/aliases.zsh
@@ -0,0 +1,30 @@
+## ALIASES
+
+# colors
+alias ls='ls --color=auto'
+unset RPROMPT
+
+# alias for yay/pacman
+alias ins='yay -S'
+alias un='yay -Rns'
+alias up='yay -Syyu --noconfirm'
+alias clean='yay -Scc --noconfirm'
+
+# git alias for my dotfiles folder
+alias dotfiles='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+
+# alias for nvim
+alias v='nvim'
+alias vim='nvim'
+alias vi='nvim'
+alias oldvim='vim'
+
+# alias for youtube-dlp
+alias mp3='yt-dlp --extract-audio --audio-format mp3'
+alias mp4='yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"'
+
+# useful shit
+alias mkdir='mkdir -p'
+alias la='ls -a'
+alias freeram="sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
+alias bm='sh $HOME/.config/hypr/scripts/bookmarks'
