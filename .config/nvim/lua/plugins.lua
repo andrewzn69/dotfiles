782e3a4c184fd57b17e78a08005d955e5c06fdb7
andrewzn <ondrejzeman23@gmail.com>
2023-10-03 01:47:27 +0200

    feat(nvim): lazy
    Closes #2

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -10,7 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
   })
 end
 vim.opt.rtp:prepend(lazypath)
-
+-- ksdfjlsdjfl kdsjlk
 
 local plugins = {
   'ellisonleao/gruvbox.nvim',                   -- color scheme
