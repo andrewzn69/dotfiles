2fb7e4e3dc47602a93f209291898dd23eb8d1196
andrewzn <ondrejzeman23@gmail.com>
2023-11-19 14:25:02 +0100

    feat(zsh): add new aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -4,7 +4,7 @@
 alias ls='ls --color=auto'
 unset RPROMPT
 
-# alias for yay/pacman
+# yay/pacman
 alias ins='yay -S'
 alias un='yay -Rns'
 alias up='yay -Syyu --noconfirm'
@@ -13,15 +13,20 @@ alias clean='yay -Scc --noconfirm && yay -Rns $(yay -Qdtq) --noconfirm'
 # git alias for my dotfiles folder
 alias d='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
 
-# alias for nvim
+# nvim
 alias v='nvim'
 alias vim='nvim'
 alias vi='nvim'
 alias oldvim='vim'
 
-# alias for youtube-dlp
+# youtube-dlp
 alias mp3='yt-dlp --extract-audio --audio-format mp3'
 alias mp4='yt-dlp -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4"'
+
+# wifi
+alias 11='fping -l 1.1.1.1 archlinux.org google.com 192.168.192.168'
+alias wconnect='nmcli dev wifi connect'
+alias wscan='nmcli dev wifi rescan'
 
 # useful shit
 alias mkdir='mkdir -p'
@@ -29,3 +34,6 @@ alias la='ls -a'
 alias freeram="sudo sh -c 'sync; echo 3 > /proc/sys/vm/drop_caches'"
 alias bm='sh $HOME/.config/hypr/scripts/bookmarks'
 alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
+alias wttr='curl wttr.in'
+
+alias calc='speedcrunch'
