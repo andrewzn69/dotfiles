1e9375c79f0321f55305b431dd2ec1715866859a
andrewzn <ondrejzeman23@gmail.com>
2024-04-29 09:21:03 +0200

    feat(nvim): add marksman lsp

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -82,6 +82,7 @@ return {
 			html = {},
 			jsonls = {},
 			lua_ls = require("plugins.lsp.servers.luals")(on_attach),
+			marksman = require("plugins.lsp.servers.marksman")(on_attach),
 			omnisharp = require("plugins.lsp.servers.omnisharp")(on_attach),
 			intelephense = require("plugins.lsp.servers.phpls")(on_attach),
 			pylsp = {},
