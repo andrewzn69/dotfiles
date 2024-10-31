3b34ced1f46c5ad8f949126d57ec25d60d51bb4c
andrewzn <ondrejzeman23@gmail.com>
2024-10-31 03:05:10 +0100

    Update aliases.zsh with new aliases and functions
    - Added alias for git commitizen (cz)
    - Added alias for termbin pastebin (pastebin)
    - Added alias for yearly calendar view (cal)
    - Added alias for cleaning dotfiles (cleandots)
    - Added alias for random filenames script (randomfilenames)
    - Moved md function to the bottom and added it back
    - Added compress function for ffmpeg to compress various file formats
    - Removed redundant blank lines and adjusted existing aliases

--- .config/zsh/aliases.zsh
+++ .config/zsh/aliases.zsh
@@ -10,8 +10,9 @@ alias un='yay -Rns'
 alias up='yay -Syyu --noconfirm'
 alias clean='yay -Scc --noconfirm && yay -Rns $(yay -Qdtq) --noconfirm'
 
-# git alias for my dotfiles folder
+# git
 alias d='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias cz='gcz'
 
 # nvim
 alias v='nvim'
@@ -36,18 +37,15 @@ alias wttr='curl wttr.in'
 alias rm='rm -iv'
 alias mv='mv -iv'
 alias cp='cp -iv'
-
-md() {
-	mkdir -p "$@" ;
-	cd "$@" ;
-}
+alias pastebin='nc termbin.com 9999'
 
 # programs
 alias qr='cobang'
 alias webcam='mpv av://v4l2:/dev/video0 --profile=low-latency --untimed'
+alias cal='cal -y'
 alias calc='speedcrunch'
-alias cal='calcurse'
 alias py='python3'
+alias cleandots='antidot clean'
 
 # scripts
 alias scrape='bash $HOME/Development/bash-scripts/scrape/scrape.sh'
@@ -57,8 +55,44 @@ alias gid='bash $HOME/Development/bash-scripts/gid/gid.sh'
 alias manual='glow $HOME/docs/Manual/'
 alias diskhealth='sudo sh $HOME/Development/bash-scripts/diskhealth/diskhealth.sh'
 alias compare='bash $HOME/Development/bash-scripts/compare/compare.sh'
+alias randomfilenames='py $HOME/Development/Python/randomfilenames/randomfilenames.py'
 
 # kubernetes
 alias h='helm'
 alias k='kubectl'
 alias kn='kubectl config set-context --current --namespace'
+
+# scripts that i did not want to put in separate files
+
+md() {
+	mkdir -p "$@"
+	cd "$@"
+}
+
+# ffmpeg compress
+compress() {
+	if [[ -z "$1" ]]; then
+		echo "Usage: compress <input_file>"
+		return 1
+	fi
+
+	input_file="$1"
+	extension="${input_file##*.}"
+	output_file="${input_file%.*}_compressed.${extension}"
+
+	case "$extension" in
+	mp4 | mkv | avi)
+		ffmpeg -i "$input_file" -vcodec libx265 -crf 28 "$output_file"
+		;;
+	jpg | jpeg | png)
+		ffmpeg -i "$input_file" -compression_level 100 "$output_file"
+		;;
+	gif)
+		ffmpeg -i "$input_file" -vf "scale=iw/2:ih/2" "$output_file"
+		;;
+	*)
+		echo "Unsupported file format: $extension"
+		return 1
+		;;
+	esac
+}
